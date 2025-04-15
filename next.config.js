83f847d16c09025659ae503a549a102dd12313cb
Oyake Sotaro-8 <oyaken1112@gmail.com>
2025-04-15 23:03:23 +0900

    Add standalone output configuration for Azure deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     domains: [
       'hebbkx1anhila5yf.public.blob.vercel-storage.com'
